9dcc7657df7cde8ee35433cc6cdd173f0efd514c
hugo <hugo6@qq.com>
2023-04-07 17:07:22 +0800

    DecoratorEditor有毒,会导致unity崩溃

--- Assets/CommonTools/Editor/Decorator/RectTransformDecorator.cs
+++ Assets/CommonTools/Editor/Decorator/RectTransformDecorator.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Common;
-using UnityEditor;
-using UnityEngine;
-
-[CustomEditor(typeof(RectTransform), true)]
-public class RectTransformDecorator : DecoratorEditor
-{
-	public RectTransformDecorator() : base("RectTransformInspector") { }
-
-	public override void OnInspectorGUI()
-	{
-		base.OnInspectorGUI();
-
-		if (GUILayout.Button("位置取整"))
-		{
-			var rt = serializedObject.targetObject as RectTransform;
-			rt.anchoredPosition = MathHelper.RoundToInt(rt.anchoredPosition, true);
-			rt.sizeDelta = MathHelper.RoundToInt(rt.sizeDelta);
-		}
-	}
-}
+﻿// using System.Collections.Generic;
+// using System.Linq;
+// using System.Reflection;
+// using Common;
+// using UnityEditor;
+// using UnityEngine;
+//
+// [CustomEditor(typeof(RectTransform), true)]
+// public class RectTransformDecorator : DecoratorEditor
+// {
+// 	public RectTransformDecorator() : base("RectTransformInspector") { }
+//
+// 	public override void OnInspectorGUI()
+// 	{
+// 		base.OnInspectorGUI();
+//
+// 		if (GUILayout.Button("位置取整"))
+// 		{
+// 			var rt = serializedObject.targetObject as RectTransform;
+// 			rt.anchoredPosition = MathHelper.RoundToInt(rt.anchoredPosition, true);
+// 			rt.sizeDelta = MathHelper.RoundToInt(rt.sizeDelta);
+// 		}
+// 	}
+// }
